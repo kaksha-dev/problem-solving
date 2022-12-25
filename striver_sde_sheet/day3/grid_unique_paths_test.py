21654d4e35f26606784b45d5eea91b5bfdfbae0a
kaksha-dev <87679878+kaksha-dev@users.noreply.github.com>
2022-12-25 07:02:31 +0000

    Day 3 Problem 5.

---
+++ striver_sde_sheet/day3/grid_unique_paths_test.py
@@ -0,0 +1,19 @@
+""" Test cases for https://leetcode.com/problems/unique-paths/. """
+
+import unittest
+from grid_unique_paths import unique_paths
+
+class MajorityElementTest(unittest.TestCase):
+    """ Test class. """
+    def test_1(self):
+        """ Testcase 1. """
+        self.assertEqual(
+            unique_paths(3, 7), 28)
+
+    def test_2(self):
+        """ Testcase 2. """
+        self.assertEqual(
+            unique_paths(3, 2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
