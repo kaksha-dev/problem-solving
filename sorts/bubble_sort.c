5de502186a0194ba1fe22a49eba51c1bdbe5a89b
kaksha-dev <87679878+kaksha-dev@users.noreply.github.com>
2024-08-26 00:02:35 +0000

    Added Bubble Sort Implementation.

---
+++ sorts/bubble_sort.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+void exch(int arr[], int i, int j)
+{
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+void bubble_sort(int arr[], int n)
+{
+  for(int i = n-1; i > 0; i--)
+  {
+    // Bubble larger elements to end of the array.
+    for(int j = 0; j < i; j++)
+    {
+       if(arr[j] > arr[i]) exch(arr, i, j);
+    }
+  }
+}
+
+long long time_elapsed(int n)
+{
+  int *arr = (int *) malloc(n * sizeof(int));
+
+  srand(time(NULL));
+  for(int i = 0; i < n; i++) arr[i] = rand();
+
+  struct timeval before;
+  gettimeofday(&before, NULL);
+  long long before_millis = before.tv_sec*1000LL + before.tv_usec/1000;
+
+  bubble_sort(arr, n);
+
+  struct timeval after;
+  gettimeofday(&after, NULL);
+  long long after_millis = after.tv_sec*1000LL + after.tv_usec/1000;
+
+  return after_millis-before_millis;
+}
+
+void test_simple_input()
+{
+  int input[] = {5, 3, 1, 9, 7};
+  int expected_output[] = {1, 3, 5, 7, 9};
+
+  bubble_sort(input, 5);
+
+  for(int i = 0; i < 5; i++) assert(input[i] == expected_output[i]);
+}
+
+int main()
+{
+  test_simple_input();
+
+  long long t1 = time_elapsed(32000);
+  long long t2 = time_elapsed(64000);
+
+  printf("%lf", t2*1.0/t1);
+  return 0;
+}
+
+
+
